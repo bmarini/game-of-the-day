e83d9df02e1afa6599caf7e385ff01e58397bb87
Ben Marini <bmarini@gmail.com>
2010-02-14 13:06:51 -0800

    Switch to class methods until an instance is really needed

--- gotd.rb
+++ gotd.rb
@@ -5,33 +5,33 @@
 # <font size=-2>vs</font><br>
 # Christopher Ward</a><br><font size=-2>A Ward Winning Game</font><!--endgameotd-->
 
-class GameOfTheDay
-  @@pgn_cache = {}
-
-  def pgn
-    @@pgn_cache[Time.now.strftime("%Y-%m-%d")] ||= get(pgn_uri)
-  end
-  
-  def pgn_uri
-    "http://www.chessgames.com/perl/nph-chesspgn?text=1&gid=#{gid}"
-  end
-  
-  def gid
-    get('http://www.chessgames.com/').
-    match(/begingameotd--><a href="\/perl\/chessgame\?gid=(\d+)">/)[1]
-  end
-
-  def get(uri)
-    Net::HTTP.get_response( URI.parse(uri) ).body
-  end
-end
-
 get '/' do
   erb :index
 end
 
 get '/gotd' do
-  gotd = GameOfTheDay.new
-  gotd.pgn
+  GameOfTheDay.pgn
 end
 
+class GameOfTheDay
+  @@pgn_cache = {}
+
+  class << self
+    def pgn
+      @@pgn_cache[Time.now.strftime("%Y-%m-%d")] ||= get(pgn_uri)
+    end
+
+    def pgn_uri
+      "http://www.chessgames.com/perl/nph-chesspgn?text=1&gid=#{gid}"
+    end
+
+    def gid
+      get('http://www.chessgames.com/').
+      match(/begingameotd--><a href="\/perl\/chessgame\?gid=(\d+)">/)[1]
+    end
+
+    def get(uri)
+      Net::HTTP.get_response( URI.parse(uri) ).body
+    end
+  end
+end
